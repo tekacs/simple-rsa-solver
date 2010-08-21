4038025bcdd3e4c4c040f51b7a63d8e5b6904779
TechKid <mail@techkid.co.uk>
2010-08-21 22:34:11 +0100

    Tidied up and added README.

--- rsa/modtest.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env ruby
-require 'ntheory'
-
-puts NTheory.modexp(545, 503, 943)
-puts NTheory.handexp(545, 503, 943)
--- rsa/task4.rb
+++
@@ -1,5 +0,0 @@
-require 'rsaenc'
-
-def enc(m)
-  rsaenc(4708819, 62311, m, false)
-end
